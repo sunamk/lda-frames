26417a0c0694e0031596b9a5492813e1e1750dda
sunamk@gmail.com <sunamk@gmail.com@0cd5556f-5d95-ad27-f374-8f773f4dff0a>
2012-03-29 20:45:21 +0000

    Strict rules for generating relations.

--- preprocessing/transform_rels.py
+++ preprocessing/transform_rels.py
@@ -26,8 +26,10 @@ USAGE: ./transform_rels.py file.rel ACTION
     -P, --pred_max P      Maximum frequency of predicates. Default is unlimited.
     -r, --real_min r      Minimum frequency of argument realisations. Default is 1.
     -R, --real_max R      Maximum frequency of argument realisations. Default is unlimited.
+    --pred_list f         Use predicates from file f.
     --pred_blacklist f    Ignore predicates from file f. The format of the file is one
-                          predicate per line. 
+                          predicate per line.
+    --real_list f         Use realisations from file f.
     --real_blacklist f    Ignore realisations from file f. The format of the file is one
                           realisation per line. 
     
@@ -42,14 +44,19 @@ pred_max = -1
 real_min = 1
 real_max = -1
 ignore_empty = False
+pred_list_file = False
 pred_blacklist_file = False
+real_list_file = False
 real_blacklist_file = False
+pred_list = set([])
 pred_blacklist = set([])
+real_list = set([])
 real_blacklist = set([])
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], "hip:P:r:R:b:", ["help", "ignore_empty=", "pred_min=",
-        "pred_max=", "real_min=", "real_max=", "pred_blacklist=", "real_blacklist="])
+        "pred_max=", "real_min=", "real_max=", "pred_list=", "pred_blacklist=", "real_list=",
+        "real_blacklist="])
 except getopt.error, msg:
     print msg
     print "for help use --help"
@@ -73,6 +80,10 @@ for o, a in opts:
         pred_blacklist_file = a
     if o == "--real_blacklist":
         real_blacklist_file = a
+    if o == "--pred_list":
+        pred_list_file = a
+    if o == "--real_list":
+        real_list_file = a
 
 if len(args) != 2:
     print "Wrong number of parameters."
@@ -101,6 +112,20 @@ if (real_blacklist_file):
     for w in b.xreadlines():
         w = w.strip()
         if w != "": real_blacklist.add(w)
+    b.close()
+
+if (pred_list_file):
+    b = open(pred_list_file)
+    for w in b.xreadlines():
+        w = w.strip()
+        if w != "": pred_list.add(w)
+    b.close()
+
+if (real_list_file):
+    b = open(real_list_file)
+    for w in b.xreadlines():
+        w = w.strip()
+        if w != "": real_list.add(w)
     b.close()
 
 if action == "info":
@@ -136,18 +161,20 @@ if action == "info":
     print "Number of unique grammatical relation realisations: %d." % len(realisations)
     print "---"
     frequent_preds = len([w for (w,i) in predicates.items() if i >= pred_min and \
-            i <= pred_max and not w in pred_blacklist])
+             i <= pred_max and not w in pred_blacklist and (not pred_list_file or w in pred_list)])
     print "Number of predicates satisfying the given constraints %d (%d %%)." % \
             (frequent_preds, int(frequent_preds*100.0/len(predicates)))
     frequent_reals = len([w for (w,i) in realisations.items() if i >= real_min and \
-            i <= real_max and not w in real_blacklist])
+            i <= real_max and not w in real_blacklist and (not real_list_file or w in real_list)])
     print "Number of realisations satisfying the given constraints %d (%d %%)." % \
             (frequent_reals, int(frequent_reals*100.0/len(realisations)))
 
 if action == "predicate_hist":
     hist = sorted(predicates.items(), key = lambda k: -k[1])
     for p in hist:
-        if p in pred_blacklist:
+        if p[0] in pred_blacklist:
+            continue
+        if pred_list_file != False and not p[0] in pred_list:
             continue
         print "%d\t%s" % (p[1], p[0])
     sys.exit(0)
@@ -155,7 +182,9 @@ if action == "predicate_hist":
 if action == "realisation_hist":
     hist = sorted(realisations.items(), key = lambda k: -k[1])
     for p in hist:
-        if p in real_blacklist:
+        if p[0] in real_blacklist:
+            continue
+        if real_list_file != False and not p[0] in real_list:
             continue
         print "%d\t%s" % (p[1], p[0])
     sys.exit(0)
@@ -172,13 +201,14 @@ for line in f.xreadlines():
         continue
     predicate = items[0]
     if predicates[predicate] < pred_min or predicates[predicate] > pred_max or \
-            predicate in pred_blacklist:
+            predicate in pred_blacklist or (pred_list_file != False and not predicate in pred_list):
         continue
     rels = items[1:]
     skip = False
     for rel in rels:
         if realisations[rel] < real_min or realisations[rel] > real_max or \
-                (rel == "-" and ignore_empty) or rel in real_blacklist:
+                (rel == "-" and ignore_empty) or rel in real_blacklist or \
+                (real_list_file != False and not rel in real_list):
             skip = True
             break
     if not skip:
