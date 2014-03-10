4876af963301783bc56c469946cc70e8a9da40ff
sunamk <sunamk@0cd5556f-5d95-ad27-f374-8f773f4dff0a>
2014-03-10 18:38:56 +0000

    added reader for all grammatical realtions

--- trunk/preprocessing/stanford-parser/methods.py
+++ trunk/preprocessing/stanford-parser/methods.py
@@ -5,6 +5,7 @@
 
 """Definitions of supported methods for selecting frame realizations"""
 import sys
+import copy
 
 def get_id(word):
     """Returns the numeric suffix from the parsed recognized words"""
@@ -150,6 +151,120 @@ def verb(parseResult, desc=False):
             try:
                 entry = (normalize(lemmas[get_id(k) - 1], tags[get_id(k) - 1]), 
                     v[0], v[1],v[2], v[3])
+                if "" in entry or not is_verb(tags[get_id(k) - 1]):
+                    continue
+                result.append(entry)
+            except ValueError:
+                continue
+            except IndexError:
+                continue
+    return result
+        
+                    
+def verbAll(parseResult, desc=False):
+    """Generates all grammatical relations for verbs."""
+    if desc == True:
+        return ";advcl\tagent\tcsubj_csubjpass\tdobj\tiobj\tsubj_nsubjpass\tprep\tprepc\tprt\ttmod"
+    emptySlots = ["NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE"]
+
+    result = []
+    for sentence in parseResult:
+        lemmas = None
+        deps = None
+        tags = None
+        try:
+            lemmas = sentence["lemmas"]
+            deps = sentence["deps"]
+            tags = sentence["tags"]
+        except TypeError:
+            sys.stderr.write("Bad input. Ignoring line.\n")
+            continue
+        except KeyError:
+            sys.stderr.write("Bad input. Ignoring line.\n")
+            continue
+        rels = {}
+        for dep in deps:
+            l2 = ""
+            try:
+                tag = tags[get_id(dep[2]) - 1]
+                l2 = normalize(lemmas[get_id(dep[2]) - 1], tag)
+            except ValueError:
+                continue
+            except IndexError:
+                continue
+            if  dep[0] == "advcl":
+                try:
+                    rels[dep[1]][0] = l2
+                except KeyError:
+                    rels[dep[1]] = copy.copy(emptySlots)
+                    rels[dep[1]][0] = l2
+            
+            elif  dep[0] == "agent":
+                try:
+                    rels[dep[1]][1] = l2
+                except KeyError:
+                    rels[dep[1]] = copy.copy(emptySlots)
+                    rels[dep[1]][1] = l2
+            
+            elif  dep[0] == "csubj" or dep[0] == "csubjpass":
+                try:
+                    rels[dep[1]][2] = l2
+                except KeyError:
+                    rels[dep[1]] = copy.copy(emptySlots)
+                    rels[dep[1]][2] = l2
+            if  dep[0] == "dobj":
+            
+                try:
+                    rels[dep[1]][3] = l2
+                except KeyError:
+                    rels[dep[1]] = copy.copy(emptySlots)
+                    rels[dep[1]][3] = l2
+
+            elif  dep[0] == "iobj":
+                try:
+                    rels[dep[1]][4] = l2
+                except KeyError:
+                    rels[dep[1]] = copy.copy(emptySlots)
+                    rels[dep[1]][4] = l2
+
+            elif  dep[0] == "nsubj" or dep[0] == "nsubjpass" :
+                try:
+                    rels[dep[1]][5] = l2
+                except KeyError:
+                    rels[dep[1]] = copy.copy(emptySlots)
+                    rels[dep[1]][5] = l2
+            
+            elif  dep[0] == "prep":
+                try:
+                    rels[dep[1]][6] = l2
+                except KeyError:
+                    rels[dep[1]] = copy.copy(emptySlots)
+                    rels[dep[1]][6] = l2
+            
+            elif  dep[0] == "prepc":
+                try:
+                    rels[dep[1]][7] = l2
+                except KeyError:
+                    rels[dep[1]] = copy.copy(emptySlots)
+                    rels[dep[1]][7] = l2
+            
+            elif  dep[0] == "prt":
+                try:
+                    rels[dep[1]][8] = l2
+                except KeyError:
+                    rels[dep[1]] = copy.copy(emptySlots)
+                    rels[dep[1]][8] = l2
+            
+            elif  dep[0] == "tmod":
+                try:
+                    rels[dep[1]][9] = l2
+                except KeyError:
+                    rels[dep[1]] = copy.copy(emptySlots)
+                    rels[dep[1]][9] = l2
+        for k, v in rels.iteritems():
+            try:
+                entry = (normalize(lemmas[get_id(k) - 1], tags[get_id(k) - 1]), 
+                    v[0], v[1],v[2],v[3],v[4],v[5],v[6],v[7],v[8],v[9])
                 if "" in entry or not is_verb(tags[get_id(k) - 1]):
                     continue
                 result.append(entry)
